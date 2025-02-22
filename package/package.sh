934f843d2c878767b20c68f3e7f66f4ee9af674e
Ryan Coe <ryan@ryco.dev>
2025-02-21 18:57:41 -0800

    update packaging
    Signed-off-by: Ryan Coe <ryan@ryco.dev>

--- package/package.sh
+++ package/package.sh
@@ -8,10 +8,10 @@ BUILD_DIR="${ROOT_DIR}/build"
 chromium_version=$(cat ${ROOT_DIR}/ungoogled-chromium/chromium_version.txt)
 ungoogled_revision=$(cat ${ROOT_DIR}/ungoogled-chromium/revision.txt)
 
-FILE_PREFIX=ungoogled-chromium_${chromium_version}-${ungoogled_revision}
-ARCHIVE_OUTPUT="${CURRENT_DIR}/${FILE_PREFIX}_linux.tar.xz"
+FILE_PREFIX="ungoogled-chromium-${chromium_version}.${ungoogled_revision}"
+ARCHIVE_OUTPUT="${CURRENT_DIR}/${FILE_PREFIX}.tar.xz"
 
-set -eux
+set -e
 
 FILES="chrome
 chrome_100_percent.pak
@@ -34,23 +34,15 @@ vk_swiftshader_icd.json
 xdg-mime
 xdg-settings"
 
-mkdir ${CURRENT_DIR}/${FILE_PREFIX}_linux
-for i in $FILES ; do 
-    cp -r ${BUILD_DIR}/src/out/Default/$i ${CURRENT_DIR}/${FILE_PREFIX}_linux
+mkdir -p ${CURRENT_DIR}/${FILE_PREFIX}
+for i in $FILES ; do
+    cp -r ${BUILD_DIR}/src/out/Default/$i ${CURRENT_DIR}/${FILE_PREFIX}
 done
 
-(cd ${CURRENT_DIR} && tar cf ${FILE_PREFIX}_linux.tar ${FILE_PREFIX}_linux)
+(cd ${CURRENT_DIR} && tar cvf ${FILE_PREFIX}.tar ${FILE_PREFIX})
 
-rm -rf ${CURRENT_DIR}/${FILE_PREFIX}_linux && xz "${CURRENT_DIR}/${FILE_PREFIX}_linux.tar" 
+rm -rf ${CURRENT_DIR}/${FILE_PREFIX} && xz -T 0 "${CURRENT_DIR}/${FILE_PREFIX}.tar"
 
-set +eux
+set +e
 
-if [ ! -f "./pkg2appimage" ] ; then
-    wget -c "https://github.com/AppImage/AppImages/raw/master/pkg2appimage" && chmod +x ./pkg2appimage
-fi
-./pkg2appimage ./ungoogled-chromium.yaml
-
-mv out/*.AppImage ${ROOT_DIR}/${FILE_PREFIX}.AppImage
-mv "${CURRENT_DIR}/${FILE_PREFIX}_linux.tar.xz" ${ROOT_DIR}
-rm -rf out ungoogled-chromium
-
+mv "${CURRENT_DIR}/${FILE_PREFIX}.tar.xz" ${ROOT_DIR}
